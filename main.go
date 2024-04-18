ce9652558d59cb674b4ff98ff016229b8bb9907f
m.siemieniaka <marcin.siemieniaka@gmail.com>
2024-04-18 21:43:51 +0200

    rows&columns

--- main.go
+++ main.go
@@ -5,13 +5,32 @@ import (
 )
 
 func main() {
+	player := 'X'
+	gameOver := false
+	var scanerRow int
+	var scanerCol int
+
 	board := [3][3]rune{}
 	for row := 0; row < len(board); row++ {
 		for col := 0; col < len(board[row]); col++ {
 			board[row][col] = ' '
 		}
 	}
-	printBoard(board)
+
+	for !gameOver {
+		printBoard(board)
+		fmt.Printf("Player %c enter row: ", player)
+		fmt.Scan(&scanerRow)
+		fmt.Printf("Player %c enter column: ", player)
+		fmt.Scan(&scanerCol)
+
+		if player == 'X' {
+			player = 'O'
+		} else {
+			player = 'X'
+		}
+	}
+
 }
 
 func printBoard(board [3][3]rune) {
